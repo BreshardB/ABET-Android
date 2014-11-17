24aa223868d54fd29761ba6a001653585432eda0
BreshardB <breshard.busby@gmail.com>
2014-11-17 01:08:26 -0600

    Added mongo jar file to classpath

--- src/com/example/abetaccreditation/MainActivity.java
+++ src/com/example/abetaccreditation/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import com.mongodb.*;
 
 public class MainActivity extends ActionBarActivity {
 

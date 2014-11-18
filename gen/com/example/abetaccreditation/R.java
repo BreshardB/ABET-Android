1604a97922bf0220569fc430ebfd4525bdda895d
BreshardB <breshard.busby@gmail.com>
2014-11-18 13:58:38 -0600

    Attempted a MongoClient connection; need to implement a MEAN stack with API in order to allow easier access to remote server

--- gen/com/example/abetaccreditation/R.java
+++ gen/com/example/abetaccreditation/R.java
@@ -996,13 +996,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int create_form=0x7f050040;
+        public static final int data_output=0x7f050042;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1150,7 +1151,13 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int create=0x7f0a0014;
         public static final int hello_world=0x7f0a000e;
+        public static final int home_page=0x7f0a0015;
+        public static final int login=0x7f0a0012;
+        public static final int main_menu=0x7f0a0011;
+        public static final int sign_up=0x7f0a0013;
+        public static final int test_button=0x7f0a0010;
     }
     public static final class style {
         /** 
--- src/com/example/abetaccreditation/MainActivity.java
+++ src/com/example/abetaccreditation/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.abetaccreditation;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.net.UnknownHostException;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 	
 	MongoClient mongo;
+	
+	Button testButton;
+	Button loginButton;
+	Button signUpButton;
+	Button createFormButton;
+	Button homePageButton;
+	
+	TextView dataOutput;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		testButton = (Button)findViewById(R.id.test);
+		
+		dataOutput = (TextView)findViewById(R.id.data_output);
+		
+		testButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -42,4 +60,23 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.test) {
+			try {
+				mongo = new MongoClient("130.211.251.72", 27017);
+				
+				DB db = mongo.getDB("abet");
+				
+				DBCollection collection = db.getCollection("courseOutcomes");
+				DBObject myDoc = collection.findOne();
+				
+				dataOutput.setText(myDoc.toString());
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }

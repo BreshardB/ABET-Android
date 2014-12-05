27be5d99a147c677de940f0b89cdd6b01b1172b3
brendanoco1911 <bkoconnor@smu.edu>
2014-12-05 14:21:09 -0600

    form output
    made form output activity

---
+++ src/com/example/abetaccreditation/FormOutput.java
@@ -0,0 +1,13 @@
+package com.example.abet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FormOutput extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form);
+    }
+
+}
